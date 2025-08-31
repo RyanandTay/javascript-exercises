0e44b98731b9e94e9e79819194add69abe02a12b
RyanandTay <202003716+RyanandTay@users.noreply.github.com>
2025-08-30 20:21:30 -0600

    Update removeFromArray to include correct solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 const removeFromArray = function(array, removeItem) {
-    let removeIndex = array.findIndex(removeItem);
-    return array.splice(removeIndex)
+    let removeIndex = array.findIndex((currentItem) => currentItem === removeItem);
+    array.splice(removeIndex, 1)
+    return array
 };
 
 console.log(removeFromArray([1, 2, 3, 4], 3));
