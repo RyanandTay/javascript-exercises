255fa6624c26cbbee46b037bc844af06437fd5b3
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-08-30 23:21:59 -0600

    Update based on Odin solutions

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let topInt;
+    let bottomInt;
 
+    if (firstInt > secondInt) {
+        topInt = firstInt;
+        bottomInt = secondInt;
+    } else {
+        topInt = secondInt;
+        bottomInt = firstInt;
+    }
+
+    sumArray = [bottomInt];
+
+    if ((!(Number.isInteger(firstInt)) || !(Number.isInteger(secondInt)) || firstInt < 0 || secondInt < 0)) {
+        return "ERROR"
+    }
+
+    for (let i = bottomInt; i < topInt; i++) {
+        sumArray.push(i + 1);
+    }
+
+    let totalInt = 0;
+
+    return sumArray.reduce((totalInt, currentItem) => (totalInt + currentItem), 0)
 };
 
 // Do not edit below this line
