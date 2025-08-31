38737a851a6d999186995521b6d4cf2301c1b742
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-08-31 00:06:39 -0600

    Complete leapYear

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(checkLeap) {
+    if (checkLeap % 4 === 0 && checkLeap % 100 !== 0) {
+        return true
+    } else if (checkLeap % 400 === 0) {
+        return true
+    } else {
+        return false
+    }
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
