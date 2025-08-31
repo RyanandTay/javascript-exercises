ba6b260078ea66ebfe882b14b888f24c014148c8
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-08-30 18:39:32 -0600

    Complete array problems

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, removeItem) {
+    let removeIndex = array.findIndex(removeItem);
+    return array.splice(removeIndex)
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
