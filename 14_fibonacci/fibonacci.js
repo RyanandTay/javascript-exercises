567b642449f383e30b03277f82510c46c3a58d9d
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-09-09 16:55:32 -0600

    Complete exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let array = [1, 1];
+    if (+a < 0) {
+        return "OOPS";
+    } else if (+a === 0 || +a === 1) {
+        return +a;
+    } else {
+        for (let i = 2; i <= +a; i++) {
+            array.push(array[i-1] + array[i-2]);
+        }
+    }
+    
+    return array[+a - 1];
 };
 
 // Do not edit below this line
