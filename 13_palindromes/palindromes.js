9892e6095f0f01531fc4c591e5f30e8887515c01
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-09-09 15:50:29 -0600

    Complete assignment

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let originalOrder = [...string];
+    originalOrder = originalOrder.filter((item) => /[a-zA-Z0-9]/.test(item));
+    let reversedOrder = originalOrder.slice();
 
+    reversedOrder.reverse();
+    reversedOrder = reversedOrder.join("").toLowerCase();
+    originalOrder = originalOrder.join("").toLowerCase();
+
+
+    return reversedOrder === originalOrder;
 };
 
 // Do not edit below this line
