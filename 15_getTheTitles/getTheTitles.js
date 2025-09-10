179d8bd18092c13bb7d72d71d02601ee278c8791
Ryan N <202003716+RyanandTay@users.noreply.github.com>
2025-09-09 18:09:01 -0600

    Complete exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,16 @@
-const getTheTitles = function() {
+// const getTheTitles = function(objectArray) {
+//     let titleArray = [];
+//     for (const object of objectArray) {
+//         titleArray.push(object.title);
+//     }
 
-};
+//     return titleArray;
+// };
+
+const getTheTitles = function(objectArray) {
+    return objectArray.map(currentItem => currentItem.title);
+}
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
