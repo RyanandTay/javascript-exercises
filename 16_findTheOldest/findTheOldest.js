1cc901e1cbc4139ba8f1a1bd174b36cd9294843c
RyanandTay <202003716+RyanandTay@users.noreply.github.com>
2025-09-09 21:31:39 -0600

    Complete assignment

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let ageArray = array.map(current => {
+        if (!("yearOfDeath" in current)) {
+            current.age = new Date().getFullYear() - current.yearOfBirth;
+            return current;
+        } else {
+            current.age = current.yearOfDeath - current.yearOfBirth;
+            return current;
+        }
+    })
+    
+    let oldest = ageArray[0];
 
+    for (const object of ageArray) {
+        if (object.age > oldest.age) {
+            oldest = object;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
